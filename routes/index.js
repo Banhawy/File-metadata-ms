ff587c6497f56002ee5516444824e1ff08991c39
Adham EL Banhawy <the.benhawy@gmail.com>
2017-10-18 15:47:44 -0500

    Added multer update method

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const imgur = require('../services/imgur');
-const History = require('../models/history');
+const multer = require('multer');
+let upload = multer({dest: 'uploads/'})
 
 router.use(express.static('public'));
 
-router.get('/latest', (req,res)=>{
-    //Get the last 10 queries and sort on date in descending order
-    History.find({}, 'term when -_id').sort('-when').limit(10).then(results=>{
-        res.json(results);
-    });
-});
 
-router.get('/api/search/:q', (req,res)=>{
-    const query = req.params.q;
-    const offset = req.params.offset;
-    new History({ term: query }).save();
-    imgur.getImage(query, offset).then(results=>{
-        res.json(results);
-    })
+router.post ('/upload', upload.single('file'), (req,res)=>{
+    return res.json({size: req.file});
 });
 
 module.exports = router;
