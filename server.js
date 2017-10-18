7a17a2557ccbbb88dd8dfe0d2ba652210eb2455c
Adham EL Banhawy <the.benhawy@gmail.com>
2017-10-18 15:47:01 -0500

    Removed redundant dependencies

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const mongodb = require('mongodb').MongoClient;
 const cors = require('cors');
 const routes = require('./routes/index');
-const db = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 3000
@@ -13,6 +11,5 @@ const server = app.listen(port, ()=>{
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
-
 app.use(cors())
 app.use('/', routes);
